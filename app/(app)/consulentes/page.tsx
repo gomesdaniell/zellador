3034845ffafb5412d731351a9f7e515b1de0e7f1
gomesdaniell <gomes.daniell.dg@gmail.com>
2025-12-30 17:14:13 -0300

    Update page.tsx

--- app/(app)/consulentes/page.tsx
+++ app/(app)/consulentes/page.tsx
@@ -1,1 +1,9 @@
+export default function Page() {
+  return (
+    <div className="appPage">
+      <h1 className="appTitle">Consulentes</h1>
+      <p className="muted">Tela em construção.</p>
+    </div>
+  );
+}
 
