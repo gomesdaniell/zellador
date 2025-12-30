0aaee809efc876eb1c4f10c5a389596329c3aa40
gomesdaniell <gomes.daniell.dg@gmail.com>
2025-12-30 16:57:06 -0300

    Update layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,1 +1,15 @@
+import AppSidebar from "../../components/AppSidebar";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="appShell">
+      <AppSidebar />
+      <main className="appMain">
+        <div className="appCard">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+}
 
