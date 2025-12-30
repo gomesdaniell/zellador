7116d695dde20b0498f8ce7275e5ffcf812d57b4
gomesdaniell <gomes.daniell.dg@gmail.com>
2025-12-30 02:15:04 -0300

    Create ConsulentesPage component with placeholder content

---
+++ app/(app)/members/consulentes/page.tsx
@@ -0,0 +1,8 @@
+export default function ConsulentesPage() {
+  return (
+    <div className="page">
+      <h1 className="listTitle">Consulentes</h1>
+      <p className="muted">Em construção.</p>
+    </div>
+  );
+}
