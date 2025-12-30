1582f88495c744f0d75d89b1058cbf29ee3f7ae1
gomesdaniell <gomes.daniell.dg@gmail.com>
2025-12-30 17:17:05 -0300

    Update page.tsx

--- app/(app)/giras/festiva/page.tsx
+++ app/(app)/giras/festiva/page.tsx
@@ -1,1 +1,9 @@
+export default function Page() {
+  return (
+    <div className="appPage">
+      <h1 className="appTitle">Giras Festivas</h1>
+      <p className="muted">Tela em construção.</p>
+    </div>
+  );
+}
 
