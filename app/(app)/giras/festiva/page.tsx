458d8c68d4273d854f50a57ebfa56be83e7670b1
gomesdaniell <gomes.daniell.dg@gmail.com>
2025-12-30 21:03:12 -0300

    Delete app/(app)/giras/festiva directory

--- app/(app)/giras/festiva/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="appPage">
-      <h1 className="appTitle">Giras Festivas</h1>
-      <p className="muted">Tela em construção.</p>
-    </div>
-  );
-}
-
