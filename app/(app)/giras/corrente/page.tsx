ccf3b12aadf4fa696ccf3f3a94bcbd80a9da7647
gomesdaniell <gomes.daniell.dg@gmail.com>
2025-12-30 21:03:05 -0300

    Delete app/(app)/giras/corrente directory

--- app/(app)/giras/corrente/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="appPage">
-      <h1 className="appTitle">Giras Corrente</h1>
-      <p className="muted">Tela em construção.</p>
-    </div>
-  );
-}
-
