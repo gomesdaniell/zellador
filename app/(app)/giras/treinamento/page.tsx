a4d50dc80f746de7580a62b19cac03df9812a18a
gomesdaniell <gomes.daniell.dg@gmail.com>
2025-12-30 21:03:20 -0300

    Delete app/(app)/giras/treinamento directory

--- app/(app)/giras/treinamento/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="appPage">
-      <h1 className="appTitle">Giras Desenvolvimento</h1>
-      <p className="muted">Tela em construção.</p>
-    </div>
-  );
-}
-
