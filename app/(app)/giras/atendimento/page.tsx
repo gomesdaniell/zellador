3676a73bf28ff3d2e1af93392f876d0a171a791e
gomesdaniell <gomes.daniell.dg@gmail.com>
2025-12-30 21:02:57 -0300

    Delete app/(app)/giras/atendimento directory

--- app/(app)/giras/atendimento/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="appPage">
-      <h1 className="appTitle">Gira Atendimento</h1>
-      <p className="muted">Tela em construção.</p>
-    </div>
-  );
-}
-
